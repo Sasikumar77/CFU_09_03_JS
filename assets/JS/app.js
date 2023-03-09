2d52c787465e939e861fa0efc8041a7acf333a12
SK_coder <116252161+Sasikumar77@users.noreply.github.com>
2023-03-09 19:34:52 +0530

    js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -12,9 +12,6 @@ function charLength() {
 }
 
 
-
-    
-
 const btn = document.querySelector(".btn")
 
 btn.addEventListener("click",function(){
@@ -27,125 +24,34 @@ btn.addEventListener("click",function(){
     const symbols = document.getElementById("symbols")
 
 
+    let cap = "";
 
-    //uppercase
+    //for capital letters
     if (upper.checked === true) {
-        
+     cap += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    //for small letters
+    if (lower.checked === true) {
+         cap += "abcdefghijklmnopqrstuvwxyz"
+        }
+    //for numbers    
+    if (numbers.checked === true) {
+            cap += "0123456789"
+           }
+    //for symbols       
+    if (symbols.checked === true) {
+            cap += "[~! @#£€$¢¥§%^&*/()\\-_+={}[]:;\'<>,.?]"
+           }
+
+     
     let big = ""
     for(i=0;i<charLength();i++){
-
-        const cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        //create random password with math random
         big += cap[Math.floor(Math.random() * cap.length)]
 
     }
  
     const result = document.getElementById("password__result")
 
-    result.value = big
-    }
-
-    //lowercase
-    if (lower.checked === true) {
-        
-        let small = ""
-        for(i=0;i<charLength();i++){
-    
-            const cap = "abcdefghijklmnopqrstuvwxyz"
-            small += cap[Math.floor(Math.random() * cap.length)]
-    
-        }
-     
-        const result = document.getElementById("password__result")
-    
-        result.value = small
-        }
-
-        //numbers
-        if (numbers.checked === true) {
-        
-            let digit = ""
-            for(i=0;i<charLength();i++){
-        
-                const cap = "0123456789"
-                digit += cap[Math.floor(Math.random() * cap.length)]
-        
-            }
-         
-            const result = document.getElementById("password__result")
-        
-            result.value = digit
-            }
-
-            //symbols
-            if (symbols.checked === true) {
-        
-                let sym = ""
-                for(i=0;i<charLength();i++){
-            
-                    const cap = "[~! @#£€$¢¥§%^&*/()\\-_+={}[]:;\'<>,.?]"
-                    sym += cap[Math.floor(Math.random() * cap.length)]
-            
-                }
-             
-                const result = document.getElementById("password__result")
-            
-                result.value = sym
-                }
-
-                //upper and lower
-                if (upper.checked === true && lower.checked === true) {
-        
-                    let letter = ""
-                    for(i=0;i<charLength();i++){
-                
-                        const cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-                        letter += cap[Math.floor(Math.random() * cap.length)]
-                
-                    }
-                 
-                    const result = document.getElementById("password__result")
-                
-                    result.value = letter
-                    }
-
-                    //uppper and lower and number
-                    if (upper.checked === true && lower.checked === true && numbers.checked === true) {
-        
-                        let letter = ""
-                        for(i=0;i<charLength();i++){
-                    
-                            const cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-                            letter += cap[Math.floor(Math.random() * cap.length)]
-                    
-                        }
-                     
-                        const result = document.getElementById("password__result")
-                    
-                        result.value = letter
-                        }
-
-                        //upper and lower and number and symbol
-                        if (upper.checked === true && lower.checked === true && numbers.checked === true && symbols.checked === true) {
-        
-                            let letter = ""
-                            for(i=0;i<charLength();i++){
-                        
-                                const cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[~! @#£€$¢¥§%^&*/()\\-_+={}[]:;\'<>,.?]"
-                                letter += cap[Math.floor(Math.random() * cap.length)]
-                        
-                            }
-                         
-                            const result = document.getElementById("password__result")
-                        
-                            result.value = letter
-                            }
-
-    
-
-    
-    
-
-    
-    
+    result.value = big   
 })
-
